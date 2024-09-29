d5e8e9aaf02f967da5444f33cccafc57a08ad636
JoaoDutra <jaumdutra744@gmail.com>
2024-09-29 11:42:37 -0300

    Commit add column nome

--- projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -8,28 +8,39 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "usuario")
 public class Usuario {
-  
+
     @Id
-    @Column(name = "cpf", unique = true, nullable = false, length = 11)  // CPF com 11 dígitos
+    @Column(name = "cpf", unique = true, nullable = false, length = 11) // CPF com 11 dígitos
     private String cpf;
 
-    @Column(name = "telefone", length = 15, nullable = true)  // Ajustado para String, se for número grande
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "telefone", length = 15, nullable = true) // Ajustado para String, se for número grande
     private String telefone;
 
     @Column(name = "nascimento", columnDefinition = "DATE", nullable = false)
     private String nascimento;
-    
-    @Column(name = "quantidade_partidas", nullable = false)  // Corrigido para o nome correto da coluna
-    private String quantidadePartidas; 
+
+    @Column(name = "quantidade_partidas", nullable = false) // Corrigido para o nome correto da coluna
+    private String quantidadePartidas;
 
     // Getters e Setters
-   
+
     public String getCpf() {
         return cpf;
     }
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getTelefone() {
