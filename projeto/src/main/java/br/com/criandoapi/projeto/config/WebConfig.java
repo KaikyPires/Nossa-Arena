1f35dbae67f8be2ecda7a77efd8777d1429d8036
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-03-13 21:35:52 -0300

    chore: acesso deploy

--- projeto/src/main/java/br/com/criandoapi/projeto/config/WebConfig.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://127.0.0.1:5500")
+            .allowedOrigins(
+                "http://127.0.0.1:5500", // acesso local
+                "https://nossa-arena-production.up.railway.app" // acesso do deploy
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true);
